a1d5d8c02d96ff3b5ced2be96bbbbe269dd4b163
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-04-27 22:06:34 +0000

    Listing unique bands

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,11 @@
+-- Lists all bands with Glam rock as their main style, ranked by their 
+-- longevity
+
+SELECT band_name,
+       CASE WHEN formed > 0 AND split > 0 THEN 2022 - formed
+            WHEN formed > 0 AND split = 0 THEN 2022 - formed
+            ELSE 0
+       END AS lifespan
+FROM metal_bands
+WHERE style = 'Glam rock'
+ORDER BY lifespan DESC;
