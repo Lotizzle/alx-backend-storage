f2a132b6bcaf8425f5a9e391f5db96fc9971baef
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-05-02 01:13:37 +0000

    Added get methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -23,8 +23,42 @@ class Cache:
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
-        The store method return the value of a random keyy
+        The store method return the value of a random key
+
+        Parameter: data
         """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:
+        """
+        This method takes a key string argument and
+        an optional Callable argument named fn
+
+        Parameter: key, fn
+        """
+        if fn is None:
+            return self._redis.get(key)
+        else:
+            data = self._redis.get(key)
+            if data is None:
+                return None
+            else:
+                return fn(data)
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """
+        This method automatically parametrizes Cache.get to
+        return a string object
+
+        Parameter: key
+        """
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """
+        This method automatically parametrizes Cache.get
+        to return an int object
+        """
+        return self.get(key, fn=lambda d: int(d))
