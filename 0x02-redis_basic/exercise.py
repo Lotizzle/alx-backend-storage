7f3e2f0ed4ac77c2eff568c86111354cb3357ef8
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-05-27 10:25:34 +0100

    Replay

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -102,14 +102,16 @@ class Cache:
         """
         return self.get(key, fn=lambda d: int(d))
 
+    @staticmethod
     def replay(method: Callable) -> None:
-    """
-    Display the history of calls of a particular function
-    """
-    method_name = method.__qualname__
-    inputs = cache._redis.lrange(f"{method_name}:inputs", 0, -1)
-    outputs = cache._redis.lrange(f"{method_name}:outputs", 0, -1)
+        """
+        Display the history of calls of a particular function
+        """
+        cache = Cache()
+        method_name = method.__qualname__
+        inputs = cache._redis.lrange(f"{method_name}:inputs", 0, -1)
+        outputs = cache._redis.lrange(f"{method_name}:outputs", 0, -1)
 
-    print(f"{method_name} was called {len(inputs)} times:")
-    for input_args, output in zip(inputs, outputs):
-        print(f"{method_name}(*{input_args.decode('utf-8')}) -> {output.decode('utf-8')}")
+        print(f"{method_name} was called {len(inputs)} times:")
+        for input_args, output in zip(inputs, outputs):
+            print(f"{method_name}(*{input_args.decode('utf-8')}) -> {output.decode('utf-8')}")
